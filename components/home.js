d483a2a5aeb66b694f8dcf3e689f45b3ef9838af
vladimir29072002 <vladimirovichh@abv.bg>
2021-03-19 21:49:25 +0200

    add header to homepage

--- components/home.js
+++ components/home.js
@@ -1,10 +1,26 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
+const template = () => html`
+        <div class="jumbotron jumbotron-fluid text-light" style="background-color: #343a40;">
+            <img src="https://s.studiobinder.com/wp-content/uploads/2019/06/Best-M-Night-Shyamalan-Movies-and-Directing-Style-StudioBinder.jpg"
+                class="img-fluid" alt="Responsive image">
+            <h1 class="display-4">Movies</h1>
+            <p class="lead">Unlimited movies, TV shows, and more. Watch anywhere. Cancel anytime.</p>
+        </div> 
+`;
+
+
 class Home extends HTMLElement {
     constructor() {
         super();
         console.log(`Home`);
     }
     connectedCallback() {
-        this.innerHTML = 'Home component'
+        this.render();
+    }
+
+    render() {
+        render(template(), this);
     }
 }
 
